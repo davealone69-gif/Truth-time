1fd46114cb0f14f86b925bfb996d6484e2a936fd
davealone69-gif <davealone69@gmail.com>
2026-08-19 19:28:42 +0800

    Use version catalog plugins and core-ktx for :libs

--- libs/build.gradle.kts
+++ libs/build.gradle.kts
@@ -1,34 +1,36 @@
 plugins {
-  id("com.android.library")
-  id("org.jetbrains.kotlin.android")
+  alias(libs.plugins.android.library)
+  alias(libs.plugins.kotlin.android)
 }
 
 android {
   namespace = "com.example.libs"
   compileSdk = 35
-  
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-  }
-  
-  kotlinOptions { jvmTarget = "17" }
 
   defaultConfig {
     minSdk = 24
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("consumer-rules.pro")
   }
+
   buildTypes {
     release {
       isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+      proguardFiles(
+          getDefaultProguardFile("proguard-android-optimize.txt"),
+          "proguard-rules.pro",
+      )
     }
   }
+
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
   }
+
   kotlinOptions { jvmTarget = "17" }
 }
 
-dependencies { implementation("androidx.core:core-ktx:1.15.0") }
+dependencies {
+  implementation(libs.androidx.core.ktx)
+}
